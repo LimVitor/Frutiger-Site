7c8103517e812568681c3327b85074c0fa4f0291
Vitor Lima <vitorlima.0811@gmail.com>
2026-07-15 00:10:09 -0300

    estrutura html completa

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+document.querySelectorAll('[role="tablist"]').forEach((tablist) => {
+  const tabs = tablist.querySelectorAll('[role="tab"]');
+  const panels = tablist.parentNode.querySelectorAll('[role="tabpanel"]');
+
+  tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+      tabs.forEach((t) => t.setAttribute('aria-selected', 'false'));
+      panels.forEach((p) => (p.hidden = true));
+
+      tab.setAttribute('aria-selected', 'true');
+      const panel = document.getElementById(tab.getAttribute('aria-controls'));
+      if (panel) panel.hidden = false;
+    });
+  });
+});
+[...document.querySelectorAll('.buttons')]
+  .map((div) => div.children[0])
+  .forEach((innerDiv) => {
+    let follower = innerDiv.cloneNode(true);
+    follower.classList.add('follower');
+    innerDiv.parentElement.appendChild(follower);
+    follower.classList.add('play');
+    innerDiv.classList.add('play');
+  });
